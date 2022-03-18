87b1197a1e8e7905423d0dc02f46e245ac2928da
nandarooscm <scm.nandaroo@gmail.com>
2022-03-18 16:54:24 +0630

    first commit

---
+++ vue-frame-master/src/pages/post/PostCreate.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="mx-auto mt-5" max-width="500">
+    <v-card-title class="create-post-title primary white--text">
+      <span class="title font-weight-light">Create Post</span>
+    </v-card-title>
+    <v-form ref="form" v-model="valid" @submit.prevent="login">
+      <v-card-text>
+        <div class="login-error">{{ error }}</div>
+        <v-text-field label="Post Title" required></v-text-field>
+        <v-textarea required>
+          <template v-slot:label>
+            <div>Post Description</div>
+          </template>
+        </v-textarea>
+      </v-card-text>
+      <v-card-actions>
+        <div class="footer">
+          <v-btn type="submit" large class="primary">Comfirm</v-btn>
+          <v-btn type="reset" large >Clear</v-btn>
+        </div>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<script src="../../services/post/post-create.js">
+</script>
+
+<style>
+.footer{
+    text-align: right;
+}
+.footer button{
+    margin:0 30px;
+}
+</style>
---
+++ vue-frame-master/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Login from "../pages/user/Login";
+import PostList from "../pages/post/PostList";
+import PostCreate from "../pages/post/PostCreate";
+import store from "../store";
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/login",
+        name: "login",
+        component: Login,
+    },
+    {
+        path: "/post/list",
+        name: "post-list",
+        component: PostList,
+    },
+   /*  {
+        path: "/*",
+        redirect: "/post/list",
+    }, */
+    {
+        path: "/post/create",
+        name: "post-create",
+        component: PostCreate,
+    },
+];
+
+const router = new VueRouter({
+    mode: "history",
+    routes,
+});
+
+/**
+ * This is to handle and check authentication for routing.
+ */
+router.beforeEach((to, from, next) => {
+    const loggedIn = store.getters.isLoggedIn;
+    if (!loggedIn && to.name != "login") {
+        return next("/login");
+    }
+    next();
+});
+
+export default router;
